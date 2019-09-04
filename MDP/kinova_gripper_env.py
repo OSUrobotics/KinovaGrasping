896d49cfc0d0ad72d079d107d6de6ff212151370
YiHerngOng <ongyi@oregonstate.edu>
2019-09-03 20:10:14 -0700

    rl framework

--- MDP/kinova_gripper_env.py
+++ MDP/kinova_gripper_env.py
@@ -9,21 +9,28 @@
 
 
 import gym
+from gym import utils, spaces
+from gym.envs.mujoco import mujoco_env
 import numpy as np
 from mujoco_py import MjViewer, load_model_from_path, MjSim
 from PID_Kinova_MJ import *
 import math
 import matplotlib.pyplot as plt
 import time
-import os
+import os, sys
 
-class Kinova_MJ(object):
-	def __init__(self, arm_or_end_effector):
+# resolve cv2 issue 
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+
+class KinovaGripper_Env(gym.Env):
+	def __init__(self, arm_or_end_effector, frame_skip=20):
 		file_dir = os.path.dirname(os.path.realpath(__file__))
 		if arm_or_end_effector == "arm":
 			self._model = load_model_from_path(file_dir + "/kinova_description/j2s7s300.xml")
+			full_path = file_dir + "/kinova_description/j2s7s300.xml"
 		elif arm_or_end_effector == "hand":
 			self._model = load_model_from_path(file_dir + "/kinova_description/j2s7s300_end_effector.xml")
+			full_path = file_dir + "/kinova_description/j2s7s300_end_effector.xml"
 		else:
 			print("CHOOSE EITHER HAND OR ARM")
 			raise ValueError
@@ -47,6 +54,12 @@ class Kinova_MJ(object):
 
 		# define pid controllers for all joints
 		self.pid = [PID_(65,0.04,0.0), PID_(10,0.01,0.0), PID_(10,0.01,0.0), PID_(10,0.01,0.0)]
+		# self.pid = [PID_(10,0.01,0.0), PID_(1,0.0,0.0), PID_(1,0.0,0.0), PID_(1,0.0,0.0)]
+
+		# Parameters for cost function
+		self.state_des = 0.20 
+
+		mujoco_env.MujocoEnv.__init__(self, full_path, frame_skip)
 
 	def set_step(self, seconds):
 		self._numSteps = seconds / self._timestep
@@ -124,10 +137,30 @@ class Kinova_MJ(object):
 		return self._sim.data.get_geom_xpos("cube")
 
 
-	# def _get_reward(self, state, action):
+	def _get_done(self):
+		return False
+
+	# set reward as object being lifted 
+	# need normalize
+	def _get_dist_reward(self):
+		state = self._get_obj_pose()
+		reward = -(self.state_des - state[2]) # only get z for now
+
+		return reward
+
+	def _get_norm_reward(self):
+		state = self._get_obj_pose()
+		reward_norm = (state[2] - 0.06) / (0.2 - 0.06)
+		if reward_norm > 1.0:
+			reward_norm = 1.0
+		elif reward_norm < 0.0:
+			reward_norm = 0.0
+
+		return reward_norm
 
 
 	def step(self, action, render=False):
+		print("control range", self._model.actuator_ctrlrange.copy())
 		initial_handpose = np.zeros(7)
 		self._sim.data.qpos[0:7] = initial_handpose 
 		initial_fingerpose = np.array([0.0, 0.0, 0.0, 0.0])
@@ -138,48 +171,40 @@ class Kinova_MJ(object):
 		start = time.time()
 
 
-		for _ in range(500):
-			# print(self._sim.data.sensordata[:])
-			# print("f1p:",self._sim.data.get_geom_xpos("f2_prox"))
-			# print("object:",type(self._sim.data.get_geom_xpos("cube")))
-			# print(np.append(self._sim.data.get_geom_xmat("f1_prox")[0], self._sim.data.get_geom_xpos("f1_prox")[0]))
-			pose = self._get_finger_pose("global")
-			
-			# print("here:",self._sim.data.get_site_xpos("f1_prox")[:])
-			# print("here:",self._sim.data.sensordata[4],self._sim.data.sensordata[5], self._sim.data.sensordata[6], self._sim.data.sensordata[7], self._sim.data.sensordata[8] )
-			range_data = self._get_rangefinder_data()
-
-			self._wrist_control()
-			self._finger_control()
-			# if step > 1000:				
-			target = np.array(action) # rad 
-			target_vel = np.zeros(3) + target
-			target_delta = target / 500
-			# print(np.max(np.abs(gripper[1:] - target_vel)) > 0.01)
-			if np.max(np.abs(gripper[1:] - target_vel)) > 0.001:
-				# print("curling in")
-				gripper[1:] += target_delta
+		pose = self._get_finger_pose("global")
+		
+		range_data = self._get_rangefinder_data()
+		self._wrist_control() # wrist action
+		self._finger_control() # finger action
+		# if step > 1000:				
+		target = np.array(action) # rad 
+		target_vel = np.zeros(3) + target
+		target_delta = target / 500
+		# print(np.max(np.abs(gripper[1:] - target_vel)) > 0.01)
+		if np.max(np.abs(gripper[1:] - target_vel)) > 0.001:
+			# print("curling in")
+			gripper[1:] += target_delta
+			self.set_target_thetas(gripper)
+		else: # grasp validation
+			wrist_target = 0.2
+			wrist_delta = wrist_target / 500
+			if abs(gripper[0] - wrist_target) > 0.001:
+				gripper[0] += wrist_delta
 				self.set_target_thetas(gripper)
-			else: # grasp validation
-				wrist_target = 0.2
-				wrist_delta = wrist_target / 500
-				if abs(gripper[0] - wrist_target) > 0.001:
-					gripper[0] += wrist_delta
-					self.set_target_thetas(gripper)
-				else:
-					print("time_spent:", time.time() - start)		
 
-			step += 1
-			self._sim.step()
-			if render:
-				self._viewer.render()
+		# step += 1
+		self._sim.step() # update every 0.002 seconds (500 Hz)
+		if render:
+			self._viewer.render()
 
+		print("reward", self._get_norm_reward())
+		reward = self._get_norm_reward()
+		print("obj pose", self._get_obj_pose())
 
-
-
+		return 
 
 if __name__ == '__main__':
-	print(os.path.dirname(os.path.realpath(__file__)))
-	sim = Kinova_MJ("hand")
+	# print(os.path.dirname(os.path.realpath(__file__)))
+	sim = KinovaGripper_Env("hand")
 	# data = sim.physim_mj()
 	sim.step([0.8, 0.8, 0.8], True)
