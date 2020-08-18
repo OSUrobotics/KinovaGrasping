7f28c362a683b0861d1b5d4ba46e21841855146e
hugheste <hugheste@oregonstate.edu>
2020-08-17 22:42:38 -0700

    Fixed max episode number

--- gym-kinova-gripper/main_DDPGfD.py
+++ gym-kinova-gripper/main_DDPGfD.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
 	parser.add_argument("--start_timesteps", default=100, type=int)		# How many time steps purely random policy is run for
 	parser.add_argument("--eval_freq", default=100, type=float)			# How often (time steps) we evaluate
 	parser.add_argument("--max_timesteps", default=1e6, type=int)		# Max time steps to run environment for
-	parser.add_argument("--max_episode", default=2000, type=int)		# Max time steps to run environment for
+	parser.add_argument("--max_episode", default=20000, type=int)		# Max time steps to run environment for
 	parser.add_argument("--save_models", action="store_true")			# Whether or not models are saved
 	parser.add_argument("--expl_noise", default=0.1, type=float)		# Std of Gaussian exploration noise
 	parser.add_argument("--batch_size", default=250, type=int)			# Batch size for both actor and critic
@@ -334,7 +334,7 @@ if __name__ == "__main__":
 
 		# Fill training object list using latin square
 		if (env.check_obj_file_empty("objects.csv")):
-			env.Generate_Latin_Square(args.max_episode,"objects.csv",shape_keys)
+			env.Generate_Latin_Square(args.max_episode,"objects.csv",shape_keys=requested_shapes)
 		state, done = env.reset(env_name="env",shape_keys=requested_shapes,hand_orientation=requested_orientation,mode=args.mode), False
 
 		noise.reset()
