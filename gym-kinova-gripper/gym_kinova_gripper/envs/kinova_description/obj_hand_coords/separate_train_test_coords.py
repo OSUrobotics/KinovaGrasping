dfac13340d79bfcf3242d5563300c1ad692d64cc
hugheste <hugheste@oregonstate.edu>
2021-05-28 09:38:44 -0700

    Moved the separate_train_test_coords.py to the general object_hand_coords directory for use by either set of coordinates (non onoise or with noise)

--- gym-kinova-gripper/gym_kinova_gripper/envs/kinova_description/obj_hand_coords/separate_train_test_coords.py
+++ gym-kinova-gripper/gym_kinova_gripper/envs/kinova_description/obj_hand_coords/separate_train_test_coords.py
@@ -6,7 +6,7 @@ import argparse
 
 """ Takes in a full dataset of coordinates (x,y,z) from txt file and splits into train and test sets """
 
-def split_coords(all_data_filepath,with_noise):
+def split_coords(all_data_filepath,with_noise,split_type):
     """ Split full coordinate dataset into train and test sets
     all_data_filepath: Filepath to full dataset to be split (Ex: all_shapes/)
     """
@@ -15,11 +15,15 @@ def split_coords(all_data_filepath,with_noise):
     if not os.path.isdir(train_coords_dir):
         os.mkdir(train_coords_dir)
 
+    eval_coords_dir = "./eval_coords"
+    if not os.path.isdir(eval_coords_dir):
+        os.mkdir(eval_coords_dir)
+
     test_coords_dir = "./test_coords"
     if not os.path.isdir(test_coords_dir):
         os.mkdir(test_coords_dir)
 
-    orientations = ["normal","roatated","top"]
+    orientations = ["normal","rotated","top"]
 
     for hand_orientation in orientations:
         for coords_file in glob.glob(all_data_filepath+hand_orientation+'/*.txt'):
@@ -39,37 +43,53 @@ def split_coords(all_data_filepath,with_noise):
                         # Hand orientation is set to (0, 0, 0) if no orientation is selected
                         data.append([float(i[0]), float(i[1]), float(i[2]), 0, 0, 0])
 
-            # Split data into train (90%) and test (10%) sets
-            train_coords = []
-            amount_training = len(data) * 0.9
-            train_coords = [data.pop(random.randrange(len(data))) for _ in range(int(amount_training))]
-
             coords_file = os.path.basename(coords_file)
-            train_coords_path = train_coords_dir+"/"+hand_orientation+"/"
-            if not os.path.isdir(train_coords_path):
-                os.mkdir(train_coords_path)
 
-            # Write train orientation noise to file
-            with open(train_coords_path+coords_file, 'w', newline='') as outfile:
-                w_train = csv.writer(outfile)
-                for coords in train_coords:
-                    w_train.writerow(coords)
+            if split_type == "eval":
+                eval_coords = [data.pop(random.randrange(len(data))) for _ in range(500)]
 
-            test_coords_path = test_coords_dir+"/"+hand_orientation+"/"
-            if not os.path.isdir(test_coords_path):
-                os.mkdir(test_coords_path)
+                eval_coords_path = eval_coords_dir + "/" + hand_orientation + "/"
+                if not os.path.isdir(eval_coords_path):
+                    os.mkdir(eval_coords_path)
 
-            # Write test orientation noise to file
-            with open(test_coords_path+coords_file, 'w', newline='') as outfile:
-                w_test = csv.writer(outfile)
-                for coords in data:
-                    w_test.writerow(coords)
+                # Write train orientation noise to file
+                with open(eval_coords_path + coords_file, 'w', newline='') as outfile:
+                    w_train = csv.writer(outfile)
+                    for coords in eval_coords:
+                        w_train.writerow(coords)
+
+            elif split_type == "train_test":
+                # Split data into train (90%) and test (10%) sets
+                train_coords = []
+                amount_training = len(data) * 0.9
+                train_coords = [data.pop(random.randrange(len(data))) for _ in range(int(amount_training))]
+
+                train_coords_path = train_coords_dir + "/" + hand_orientation + "/"
+                if not os.path.isdir(train_coords_path):
+                    os.mkdir(train_coords_path)
+
+                # Write train orientation noise to file
+                with open(train_coords_path + coords_file, 'w', newline='') as outfile:
+                    w_train = csv.writer(outfile)
+                    for coords in train_coords:
+                        w_train.writerow(coords)
+
+                test_coords_path = test_coords_dir+"/"+hand_orientation+"/"
+                if not os.path.isdir(test_coords_path):
+                    os.mkdir(test_coords_path)
+
+                # Write test orientation noise to file
+                with open(test_coords_path+coords_file, 'w', newline='') as outfile:
+                    w_test = csv.writer(outfile)
+                    for coords in data:
+                        w_test.writerow(coords)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--all_data_filepath", type=str, action='store', default="./")
     parser.add_argument("--with_orientation_noise", type=str, action='store', default="True")
+    parser.add_argument("--split_type", type=str, action='store', default="train_test")
     args = parser.parse_args()
 
     if args.with_orientation_noise == "True":
@@ -78,4 +98,4 @@ if __name__ == "__main__":
         with_orientation_noise = False
 
     # Split coordinates from the full dataset into train and test sets
-    split_coords(args.all_data_filepath, with_orientation_noise)
+    split_coords(args.all_data_filepath, with_orientation_noise, args.split_type)
