ad643fd637707f50eed65e036c718cf3e0b29a1b
Nigel Swenson <swensoni@oregonstate.edu>
2021-09-27 13:48:09 -0700

    added episode class base and updated timestep class base

--- gym-kinova-gripper/episode_class_base.py
+++ gym-kinova-gripper/episode_class_base.py
@@ -5,20 +5,75 @@ Created on Thu Sep 23 14:16:32 2021
 
 @author: orochi
 """
+from collections import OrderedDict
+from timestep_class_base import TimestepBase
+
 
 class EpisodeBase():
-    
-    def __init__(self):
-        
-    
-    def get_timestep(self,timestep):
-        return 0
-    
+    """ Episode class contains the timesteps from a single episode, methods to
+    acces the data and a method to save it in a csv file
+    @param shape - shape being picked up
+    @param size - size of shape being grabbed
+    @param orientation - orientation of grasp
+    @param episode_num - current episode if running multiple"""
+    def __init__(self, shape='', size='', orientation='', episode_num=0):
+        self.data = OrderedDict()
+        self.episode_num = episode_num
+        self.shape = shape
+        self.size = size
+        self.orientation = orientation
+
+    def get_timestep(self, timestep):
+        """Method to return the data in the timestep associated with the string
+        or int given
+        @param timestep - int or string of desired timestep"""
+        try:
+            data = self.data[timestep]
+        except KeyError:
+            key = 't_'+str(timestep)
+            data = self.data[key]
+        return data.get_full_timestep()
+
     def get_full_episode(self):
-        return 0
-    
-    def save_episode(self):
-        pass
-    
-    def set_timestep(self,timestep):
-        pass
+        """Method to return the data in all the timesteps in an ordered dict"""
+        data = OrderedDict()
+        for k, v in self.data.items():
+            data[k] = v.get_full_timestep()
+        return data
+
+    def save_episode(self, file_name=None):
+        """Method to save the episode
+        @param file_name - name of file"""
+        flag = True
+        if file_name is None:
+            file_name = 'episode_' + str(self.episode_num) + self.shape +\
+                        self.size + self.orientation + '.csv'
+        for i in self.data.values():
+            if flag:
+                i.save_timestep(file_name=file_name, write_flag='w')
+                flag = False
+            else:
+                i.save_timestep(file_name=file_name, write_flag='a')
+
+    def add_timestep(self, timestep):
+        """Method to add a new timestep to the episode
+        @param timestep - Timestep class containing all relevant data"""
+        self.data['t_' + str(timestep.times['timestep'])] = timestep
+
+
+if __name__ == '__main__':
+    a = EpisodeBase(shape='fake_cube', episode_num=1)
+    state = [1, 2, 3]
+    action = [23]
+    reward = [155]
+    timestep = 0
+    sim_time = 0.02
+    b = TimestepBase(state, action, reward, timestep, sim_time)
+    a.add_timestep(b)
+    b = TimestepBase(state, action, reward, 1, 0.04)
+    a.add_timestep(b)
+    b = TimestepBase(state, action, reward, 2, 0.06)
+    a.add_timestep(b)
+    b = TimestepBase(state, action, reward, 3, 0.08)
+    a.add_timestep(b)
+    a.save_episode()
--- gym-kinova-gripper/timestep_class_base.py
+++ gym-kinova-gripper/timestep_class_base.py
@@ -6,15 +6,9 @@ Created on Thu Sep 23 13:06:46 2021
 @author: orochi
 """
 import os
-import sys
 import time
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from core_classes.stats_tracker_base import StatsTrackerArray
-from collections import OrderedDict
-import state_space as sim_state
-import action_class_base as sim_action
-import reward_base as sim_reward
 import csv
+
 
 class TimestepBase():
 
@@ -31,18 +25,23 @@ class TimestepBase():
         self.state = state
         self.action = action
         self.reward = reward
-        self.times = {'wall_time': time.time(), 'sim_time': sim_time, 'timestep': timestep}
+        self.times = {'wall_time': time.time(), 'sim_time': sim_time,
+                      'timestep': timestep}
 
     def get_state(self):
+        """Method to return the state data"""
         return self.state.get_obs()
 
     def get_action(self):
+        """Method to return the action data"""
         return self.action.get_action()
 
     def get_reward(self):
+        """Method to return the reward data"""
         return self.reward.get_reward()
 
     def get_full_timestep(self):
+        """Method to return all stored data as one dictionary"""
         data = {}
         data['state'] = self.state.get_obs()
         data['action'] = self.action.get_action()
@@ -51,28 +50,51 @@ class TimestepBase():
         return data
 
     def set_state(self, new_state):
+        """Method to set the state
+        @param state - State class containing info from this timestep"""
         self.state = new_state
 
     def set_action(self, new_action):
+        """Method to set the action
+        @param acton - Action class containing info from this timestep"""
         self.action = new_action
 
     def set_reward(self, new_reward):
+        """Method to set the reward
+        @param reward - Reward class containing info from this timestep"""
         self.reward = new_reward
 
     def set_time(self, timestep, sim_time, wall_time=None):
+        """Method to set the time
+        @param timestep - int represting the current timestep
+        @param sim_time - float representing seconds since sim started in
+        simulator
+        @param wall_time - float represeting current computer time"""
         if wall_time is None:
-            self.times = {'wall_time': time.time(), 'sim_time': sim_time, 'timestep': timestep}
+            self.times = {'wall_time': time.time(), 'sim_time': sim_time,
+                          'timestep': timestep}
         else:
-            self.times = {'wall_time': wall_time, 'sim_time': sim_time, 'timestep': timestep}
+            self.times = {'wall_time': wall_time, 'sim_time': sim_time,
+                          'timestep': timestep}
 
-    def save_timestep(self):
-        path = os.path.abspath(__file__)
-        file_name = path + '/data/' + 'tstep_' + self.times['timestep'] + '_wall_time_' + time.strftime("%m-%d-%y %H:%M:%S", time.localtime(self.times['wall_time'])) + '.csv'
+    def save_timestep(self, file_name=None, write_flag='w'):
+        """Method to save the timestep in a csv file
+        @param file_name - name of file
+        @param write_flag - type of writing, defaults to write but can be set
+        to 'a' to append to the file instead"""
+        path = os.path.dirname(os.path.abspath(__file__))
+        if file_name is None:
+            file_name = path + '/data/' + 'tstep_' + self.times['timestep'] +\
+            '_wall_time_' + time.strftime("%m-%d-%y %H:%M:%S", time.localtime(self.times['wall_time'])) + '.csv'
+        else:
+            file_name = path + '/data/' + file_name
         data = self.get_full_timestep()
-        with open(file_name,'w', newline='') as csv_file:
-            time_writer = csv.writer(csv_file, delimiter=',', quotechar='|', 
+        with open(file_name, write_flag, newline='') as csv_file:
+            time_writer = csv.writer(csv_file, delimiter=',', quotechar='|',
                                      quoting=csv.QUOTE_MINIMAL)
-            time_writer.writerow(['Wall time'] + [data['wall_time']] + ['Sim time'] + [data['sim_time']] + ['Timestep'] + [data['timestep']])
+            time_writer.writerow(['Wall time'] + [data['wall_time']] +
+                                 ['Sim time'] + [data['sim_time']] +
+                                 ['Timestep'] + [data['timestep']])
             time_writer.writerow(['State'] + data['state'])
             time_writer.writerow(['Action'] + data['action'])
             time_writer.writerow(['reward'] + data['reward'])
